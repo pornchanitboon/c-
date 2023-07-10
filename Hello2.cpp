d71448c06d5d0ac7f52ad5bd347f7d3b672f78aa
pornchanitboon <139094182+pornchanitboon@users.noreply.github.com>
2023-07-10 10:38:15 +0700

    Add files via upload

---
+++ Hello2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{	cout<<"Student Code : 66-060216-2115-1"<<endl;
+    cout<<"Student Name : Pornchanit Boonsaeng"<<endl;
+	cout<<"    Division : information Technology"<<endl;
+    cout<<"        year : 1"<<endl;
+	cout<<"        Room : RB"<<endl;
+	cout<<"         sex : Female"<<endl;
+	cout<<"         age : 18"<<endl;
+	system("pause");
+    return (0);
+}
