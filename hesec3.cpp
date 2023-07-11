748ca348422bb03260e427ac7c4f54a6a0fb6b06
pornchanitboon <139094182+pornchanitboon@users.noreply.github.com>
2023-07-11 10:28:25 +0700

    Add files via upload

--- hesec3.cpp
+++ hesec3.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 int main()
-{   cout <<"  ******   "<<endl;
-    cout <<"**      *  "<<endl;
-	cout <<"**         "<<endl;
-    cout <<"**         "<<endl;
-	cout <<"**         "<<endl;
-    cout <<"**      *  "<<endl;
-	cout <<"  ******       "<<endl;
+{   cout <<"    ******   "<<endl;
+    cout <<"  **      *  "<<endl;
+cout <<"  **         "<<endl;
+    cout <<"  **         "<<endl;
+cout <<"  **         "<<endl;
+    cout <<"  **      *  "<<endl;
+cout <<"    *****       "<<endl;
 	system("pause");
 	return 0;
 }	
