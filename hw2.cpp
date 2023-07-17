f7bb525f7a3e8deda9ed2d816eb4e3374c89a200
pornchanitboon <139094182+pornchanitboon@users.noreply.github.com>
2023-07-17 10:02:01 +0700

    Add files via upload

---
+++ hw2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{   
+	string Name;
+	int Yob;
+	cout <<" Enter name : ";
+	cin  >>  Name;
+	cout <<" Yob : ";
+	cin  >>  Yob;
+	cout <<" Your Age " << 2566-Yob << " year old " <<endl;
+	
+	return 0;
+}
