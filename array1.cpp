6d6d1304cc53b3f7eae3a6080d795ec7c52fb6f0
pornchanitboon <139094182+pornchanitboon@users.noreply.github.com>
2023-09-18 11:50:52 +0700

    Add files via upload

--- array1.cpp
+++ array1.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <iomanip>
 using namespace std;
+void check_result(int x);
 int main()
 {
 	int score[4], total = 0 , max = 0 , minS = 0;
@@ -15,13 +16,15 @@ int main()
 		cin>>score[i];
 		
 	}
-	cout<<setfill('-')<<setw(30)<<" "<<endl;
-	cout<<"Name \t \t Score"<<endl;
-	cout<<setfill('-')<<setw(30)<<" "<<endl;
+	cout<<setfill('-')<<setw(60)<<" "<<endl;
+	cout<<"Name \t \t Score \t \t Result"<<endl;
+	cout<<setfill('-')<<setw(60)<<" "<<endl;
 	
 	for(int i = 0 ; i < 4 ; i++)
 	{
-		cout<<name[i]<<"\t\t"<<score[i]<<endl;
+		cout<<name[i]<<"\t\t"<<score[i]<<"\t\t";
+		check_result(score[i]);
+
 		total = total+score[i];
 		if(score[i] > max)
 			max = score[i];
@@ -31,7 +34,7 @@ int main()
 			minS = score[i];
 
 	}
-	cout<<setfill('-')<<setw(30)<<""<<endl;
+	cout<<setfill('-')<<setw(60)<<""<<endl;
 	cout<<"Max score = "<<max<<endl;
 	cout<<"Min score = "<<minS<<endl;
 	cout<<"Total     = "<<total<<endl;
@@ -39,4 +42,11 @@ int main()
 	
 	system("pause");
 	return 0 ;
+}
+void check_result(int x)
+{
+	if(x >= 50)
+		cout<<"Pass"<<endl;
+	else
+		cout<<"Fail"<<endl;
 }
