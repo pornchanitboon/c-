2e9a9f0c4a1173ccbe1652158fb008eb0b7b6ded
pornchanitboon <139094182+pornchanitboon@users.noreply.github.com>
2023-12-04 11:59:49 +0700

    Add files via upload

---
+++ information.java
@@ -0,0 +1,13 @@
+public class information {
+    public static void main(String[] args)  
+    {
+        System.out.println("Id : 63-060216-9999-0");
+        System.out.println("Name : somchai");
+        System.out.println("SurName : Cheingpongpan");
+        System.out.println("Gender: Male");
+        System.out.println("Age: 25");
+       
+    }
+}
+
+
