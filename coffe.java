dfb2a50a00b481e3e673c968fa206241dd7b03e2
pornchanitboon <139094182+pornchanitboon@users.noreply.github.com>
2023-12-04 11:55:13 +0700

    Add files via upload

---
+++ coffe.java
@@ -0,0 +1,14 @@
+public class coffe {
+    public static void main(String[] args)
+    {
+        System.out.println("     ee     ee ");
+        System.out.println("    ff     ff");
+        System.out.println("   Co     Co");
+        System.out.println("#################");
+        System.out.println("#  hello    #   #");
+        System.out.println("#  Java     ####");
+        System.out.println("#           #   ");
+        System.out.println(" ###########  ");
+
+    }
+}
